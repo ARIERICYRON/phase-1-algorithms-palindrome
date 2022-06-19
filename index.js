c2f45c6792e10c1d08034bd30dd8020c608f504f
arieri <ariericyron@gmail.com>
2022-06-19 22:22:17 +0300

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+  hat means if the
+  first letter is the same as last letter, and the second letter is the same
+  as the second to last letter, etc. until we reach the middle, return true.
+  iterate from the beginning to the middle
+    check each letter to the correponding letter from the end
+      if any letters don't match, return false
 */
 
 /*
@@ -11,6 +17,18 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
+function isPalindrome(word) {
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
+}
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -20,6 +38,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+  console.log("");
+  console.log("Expecting: true")
+  console.log("=>", isPalindrome("a"));
+  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
